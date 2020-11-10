44d71a397e3cace94571b50600c11ee0b2b07bd8
kento <jst34zais9102z@gmail.com>
2020-11-10 17:00:59 +0900

    componentを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
+import './App.scss';
+import NavBar from './components/NavBar';
+import Board from './components/Board';
 
 const App = () => {
-  return <>aaaa</>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Board />
+    </div>
+  );
 };
 export default App;
