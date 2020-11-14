aafe582a32b985fa0abc337d9ced8289c0665576
kento <jst34zais9102z@gmail.com>
2020-11-14 11:24:07 +0900

    subTitleを変更

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
-import '../NavBar.scss';
+import '../scss/NavBar.scss';
 
 const NavBar = () => {
   return (
     <div className="NavBar">
       <div className="title">アニメちゃんねる(仮)</div>
       <Navbar bg="light" className="bg-light justify-content-between Navbar">
-        <Navbar.Brand>アニメの話題でおしゃべり</Navbar.Brand>
+        <p className="subtitle">アニメの話題でおしゃべり★</p>
         <Form inline className="NavBarInline">
           <FormControl type="text" placeholder="トピックを検索" className=" mr-sm-2" />
           <Button>検索</Button>
